aeac62e0d597dc935913dfeeb57962b52acd2b96
Victor <fedorenko22116@gmail.com>
2018-07-12 01:04:38 +0300

    Added prerrender for VueJs

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 
 module.exports = {
     configureWebpack: {
         plugins: [
             new Dotenv({
-                path: __dirname + '/.env',
+                path: path.join(__dirname, '.env'),
                 safe: true,
                 systemvars: true,
                 silent: false
+            }),
+            new PrerenderSPAPlugin({
+                // Required - The path to the webpack-outputted app to prerender.
+                staticDir: path.join(__dirname, 'dist'),
+                // Required - Routes to render.
+                routes: [ '/', '/about' ],
             })
         ]
     }
