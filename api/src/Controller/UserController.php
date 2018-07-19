f7d458d5340e87510d8e022850a27bf5a2efc116
Victor <fedorenko22116@gmail.com>
2018-07-19 17:24:11 +0300

    Corrected UserController. Refs #4381

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -15,7 +15,7 @@ class UserController extends FOSRestController
 {
     /**
      * @Rest\View()
-     * @Rest\Get("")
+     * @Rest\Get()
      * @return array|View
      */
     public function getCurrentUser()
