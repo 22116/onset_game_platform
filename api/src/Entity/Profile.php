a6e25aeb36d2ee9ab4a34150daa32b15431f02de
Victor <fedorenko22116@gmail.com>
2018-07-19 16:33:51 +0300

    Profile, Token added

---
+++ api/src/Entity/Profile.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use App\Validator\Constraints as AppAssert;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProfileRepository")
+ * @ORM\Table(name="profile")
+ */
+class Profile
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\NotBlank()
+     * @Groups({"API"})
+     * @var string|null
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\NotBlank()
+     * @Groups({"API"})
+     * @var string|null
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Url()
+     * @AppAssert\IsFacebookUrl()
+     * @Groups({"API"})
+     * @var string|null
+     */
+    protected $facebookUrl;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @Groups({"API"})
+     * @var string|null
+     */
+    protected $description;
+
+    /**
+     * @ORM\OneToOne(targetEntity="User", orphanRemoval=true)
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Token", mappedBy="profile", cascade={"persist", "remove"})
+     * @var Token|null
+     */
+    private $token;
+
+    public function getToken(): ?Token
+    {
+        return $this->token;
+    }
+
+    public function setToken(?Token $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function getFacebookUrl(): ?string
+    {
+        return $this->facebookUrl;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function setFacebookUrl(?string $facebookUrl): self
+    {
+        $this->facebookUrl = $facebookUrl;
+
+        return $this;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
